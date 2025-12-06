e5c3916400169bb056ba5eac95dcf5a3de4a9799
Redkitt3n14 <69162335+Redkitt3n14@users.noreply.github.com>
2025-12-06 21:35:18 +0000

    Added TileManager
    TileManager currently pulls all with "undrafted" tag, then sorts them into a public 2D array for easier use later

--- Daft Draughters/Assets/Scripts/DrawTile.cs
+++ Daft Draughters/Assets/Scripts/DrawTile.cs
@@ -1,11 +1,52 @@
+using System;
 using UnityEngine;
 
 public class DrawTile : MonoBehaviour
 {
+
+    // tile array declare
+    private GameObject[] tilesIn;
+    public GameObject[,] tilesSorted;
+    GameObject current;
+    
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tilesIn = GameObject.FindGameObjectsWithTag("undrafted");
+
+        tilesSorted = new GameObject[6, 6];
+
+        // sorts tilesIn into 2D array by X and Y position for easier later use
+        for (int w = 0; w < 36; w++)
+        {
+            current = tilesIn[w];
+            // sorts it into the position of the tiles current X and Y pos
+            int X = Mathf.RoundToInt(current.transform.localPosition.x);
+            int Y = Mathf.RoundToInt(current.transform.localPosition.y);
+            tilesSorted[X,Y] = current;
+        }
+
+
+        for (int x = 0; x < 6; x++)
+        {
+            for (int y = 0; y < 6; y++)
+            {
+                Debug.Log($"Tile {tilesSorted[x,y]} at pos {x},{y}");
+                // moves all tiles by 2 on x as a test
+                //Destroy(tilesSorted[x,y]);
+                //tilesSorted[x,y].transform.Translate(2, 0, 0);
+                
+            }
+        }
+
+
+    }
+
+    private GameObject FindGameObjectsWithTag(string v)
+    {
+        throw new NotImplementedException();
     }
 
     // Update is called once per frame
