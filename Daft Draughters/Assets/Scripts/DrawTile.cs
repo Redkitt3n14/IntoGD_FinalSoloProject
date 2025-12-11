5f83bf66b0e13fa8a76ed5c0060eb34cad954c9b
Redkitt3n14 <69162335+Redkitt3n14@users.noreply.github.com>
2025-12-11 21:05:34 +0000

    Fixed border issue for tile sorting

--- Daft Draughters/Assets/Scripts/DrawTile.cs
+++ Daft Draughters/Assets/Scripts/DrawTile.cs
@@ -22,13 +22,14 @@ public class DrawTile : MonoBehaviour
         for (int w = 0; w < 36; w++)
         {
             current = tilesIn[w];
-            // sorts it into the position of the tiles current X and Y pos
+            // sorts it into the position of the tiles current X and Y pos, then moved to 6x6 array
             int X = Mathf.RoundToInt(current.transform.localPosition.x);
             int Y = Mathf.RoundToInt(current.transform.localPosition.y);
+
             tilesSorted[X,Y] = current;
         }
 
-
+        // any additional initial handling for the tiles can be done here
         for (int x = 0; x < 6; x++)
         {
             for (int y = 0; y < 6; y++)
@@ -36,7 +37,7 @@ public class DrawTile : MonoBehaviour
                 Debug.Log($"Tile {tilesSorted[x,y]} at pos {x},{y}");
                 // moves all tiles by 2 on x as a test
                 //Destroy(tilesSorted[x,y]);
-                //tilesSorted[x,y].transform.Translate(2, 0, 0);
+                tilesSorted[x,y].transform.Translate(0.1f, 0, 0);
                 
             }
         }
@@ -52,6 +53,19 @@ public class DrawTile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        // any additional initial handling for the tiles can be done here
+        for (int x = 0; x < 6; x++)
+        {
+            for (int y = 0; y < 6; y++)
+            {
+                Debug.Log($"Tile {tilesSorted[x, y]} at pos {x},{y}");
+                // moves all tiles by 2 on x as a test
+                //Destroy(tilesSorted[x,y]);
+                tilesSorted[x, y].transform.Translate(0.01f, 0, 0);
+
+            }
+        }
+
     }
 }
