a1658f141864bc42f6bd2a8f97862bef68007d9c
Redkitt3n14 <69162335+Redkitt3n14@users.noreply.github.com>
2025-12-14 14:25:55 +0000

    Added special DrawStart for starting tile
    swapped drawtile sorting from start to awake so it runs before positionmovement tries to draw the first tile

--- Daft Draughters/Assets/Scripts/DrawTile.cs
+++ Daft Draughters/Assets/Scripts/DrawTile.cs
@@ -16,7 +16,7 @@ public class DrawTile : MonoBehaviour
     
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         tilesIn = GameObject.FindGameObjectsWithTag("undrafted");
 
